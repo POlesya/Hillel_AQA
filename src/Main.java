19589f5e50bb4a5d421d7fa0c048f4a9235fd80d
POlesya <olesya.pilip@gmail.com>
2023-09-28 18:14:44 +0300

    CheckOddEven

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hooray! My first program!");
-        System.out.println("Hooray! My second program!");
+        int number = 12; // A variable that is checked for parity or oddity
+
+        // Check if a number is even or odd
+        if (number % 2 == 0) {
+            System.out.println("Even number"); // Even number
+        } else {
+            System.out.println("Odd number"); // Odd number
+        }
+
+        // Outputting "BYE" before ending the program
+        System.out.println("BYE");
     }
 }
+
+
+
+
+
