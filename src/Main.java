476f98bbaa0659db1792a2c0470d3e4858ec4106
POlesya <olesya.pilip@gmail.com>
2023-09-28 19:17:30 +0300

    Isosceles triangle

--- src/Main.java
+++ src/Main.java
@@ -4,21 +4,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Enter the first number: ");
-        double number1 = input.nextDouble();
+        System.out.println("Enter the length of the first side of the triangle:");
+        double side1 = input.nextDouble();
 
-        System.out.print("Enter the second number: ");
-        double number2 = input.nextDouble();
+        System.out.println("Enter the length of the second side of the triangle:");
+        double side2 = input.nextDouble();
 
-        double absoluteValue1 = Math.abs(number1);
-        double absoluteValue2 = Math.abs(number2);
+        System.out.println("Enter the length of the third side of the triangle:");
+        double side3 = input.nextDouble();
 
-        if (absoluteValue1 > absoluteValue2) {
-            System.out.println(number1 + " has more absolute value.");
-        } else if (absoluteValue2 > absoluteValue1) {
-            System.out.println(number2 + " has more absolute value.");
+        if (side1 == side2 || side2 == side3 || side1 == side3) {
+            System.out.println("The triangle is isosceles.");
         } else {
-            System.out.println("Both numbers have the same absolute value.");
+            System.out.println("The triangle is not isosceles.");
         }
 
         input.close();
