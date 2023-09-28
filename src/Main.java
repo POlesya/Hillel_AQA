4eb8ed7952f453c4220ffdccc18673c510802e67
POlesya <olesya.pilip@gmail.com>
2023-09-28 19:08:01 +0300

    Comparing the absolute values of two numbers

--- src/Main.java
+++ src/Main.java
@@ -4,31 +4,26 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Enter the month number (from 1 to 12): ");
-        int monthNumber = input.nextInt();
+        System.out.print("Enter the first number: ");
+        double number1 = input.nextDouble();
 
-        String monthName = switch (monthNumber) {
-            case 1 -> "JANUARY";
-            case 2 -> "FEBRUARY";
-            case 3 -> "MARCH";
-            case 4 -> "APRIL";
-            case 5 -> "MAY";
-            case 6 -> "JUNE";
-            case 7 -> "JULY";
-            case 8 -> "AUGUST";
-            case 9 -> "SEPTEMBER";
-            case 10 -> "OCTOBER";
-            case 11 -> "NOVEMBER";
-            case 12 -> "DECEMBER";
-            default -> "Not a valid month";
-        };
+        System.out.print("Enter the second number: ");
+        double number2 = input.nextDouble();
 
-        System.out.println(monthName);
+        double absoluteValue1 = Math.abs(number1);
+        double absoluteValue2 = Math.abs(number2);
+
+        if (absoluteValue1 > absoluteValue2) {
+            System.out.println(number1 + " has more absolute value.");
+        } else if (absoluteValue2 > absoluteValue1) {
+            System.out.println(number2 + " has more absolute value.");
+        } else {
+            System.out.println("Both numbers have the same absolute value.");
+        }
 
         input.close();
     }
 }
-
 
 
 
