f4314c4ce00579e84fd63a03c403697643aadd13
POlesya <olesya.pilip@gmail.com>
2023-09-28 18:58:19 +0300

    PrintMonthInWord - switch

--- src/Main.java
+++ src/Main.java
@@ -7,33 +7,23 @@ public class Main {
         System.out.print("Enter the month number (from 1 to 12): ");
         int monthNumber = input.nextInt();
 
-        if (monthNumber == 1) {
-            System.out.println("JANUARY");
-        } else if (monthNumber == 2) {
-            System.out.println("FEBRUARY");
-        } else if (monthNumber == 3) {
-            System.out.println("MARCH");
-        } else if (monthNumber == 4) {
-            System.out.println("APRIL");
-        } else if (monthNumber == 5) {
-            System.out.println("MAY");
-        } else if (monthNumber == 6) {
-            System.out.println("JUNE");
-        } else if (monthNumber == 7) {
-            System.out.println("JULY");
-        } else if (monthNumber == 8) {
-            System.out.println("AUGUST");
-        } else if (monthNumber == 9) {
-            System.out.println("SEPTEMBER");
-        } else if (monthNumber == 10) {
-            System.out.println("OCTOBER");
-        } else if (monthNumber == 11) {
-            System.out.println("NOVEMBER");
-        } else if (monthNumber == 12) {
-            System.out.println("DECEMBER");
-        } else {
-            System.out.println("Not a valid month");
-        }
+        String monthName = switch (monthNumber) {
+            case 1 -> "JANUARY";
+            case 2 -> "FEBRUARY";
+            case 3 -> "MARCH";
+            case 4 -> "APRIL";
+            case 5 -> "MAY";
+            case 6 -> "JUNE";
+            case 7 -> "JULY";
+            case 8 -> "AUGUST";
+            case 9 -> "SEPTEMBER";
+            case 10 -> "OCTOBER";
+            case 11 -> "NOVEMBER";
+            case 12 -> "DECEMBER";
+            default -> "Not a valid month";
+        };
+
+        System.out.println(monthName);
 
         input.close();
     }
